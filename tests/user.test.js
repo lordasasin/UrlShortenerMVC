cb66e14d8a279fcb234c0ec8506e3089f6fde3af
Yusuf Baskeser <yusufbaskeser2005@gmail.com>
2025-07-17 13:55:57 +0300

    new

--- tests/user.test.js
+++ tests/user.test.js
@@ -17,14 +17,14 @@ afterAll(async () => {
 
 describe("myurls", () => {
   it("GET /myurls is it working", async () => {
-    const res = await request(app).get("/user/list").send({});
+    const res = await request(app).get("/user/list");
     expect(res.statusCode).toBe(200);
-  });
+  }); 
 });
 
 describe("list", () => {
   it("GET /list is it working", async () => {
-    const res = await request(app).get("/user/myurls").send({});
+    const res = await request(app).get("/user/myurls");
     expect(res.statusCode).toBe(200);
   });
 });
